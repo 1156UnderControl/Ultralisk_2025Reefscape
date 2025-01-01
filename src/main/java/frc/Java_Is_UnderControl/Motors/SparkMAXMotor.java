4a5f32bebec6413ba634a96936c3a3ecbef70290
KlausKollerKroeff <kroeffk@icloud.com>
2025-01-01 15:53:41 -0300

    new: TalonFXController Class

--- src/main/java/frc/Java_Is_UnderControl/Motors/SparkMAXMotor.java
+++ src/main/java/frc/Java_Is_UnderControl/Motors/SparkMAXMotor.java
@@ -1,10 +1,8 @@
 package frc.Java_Is_UnderControl.Motors;
 
 import com.revrobotics.spark.SparkBase;
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 
-import java.io.ObjectInputFilter.Config;
 import java.util.function.Supplier;
 
 import com.revrobotics.REVLibError;
@@ -12,7 +10,6 @@ import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
---
+++ src/main/java/frc/Java_Is_UnderControl/Motors/TalonFXMotor.java
@@ -0,0 +1,325 @@
+package frc.Java_Is_UnderControl.Motors;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.util.datalog.BooleanLogEntry;
+import edu.wpi.first.util.datalog.StringLogEntry;
+import edu.wpi.first.wpilibj.DataLogManager;
+import frc.Java_Is_UnderControl.Logging.EnhancedLoggers.CustomDoubleLogger;
+import frc.Java_Is_UnderControl.Logging.EnhancedLoggers.CustomIntegerLogger;
+
+public class TalonFXMotor implements Motor{
+    private TalonFX motor;
+    private GravityTypeValue gravityType;
+
+    private final MotionMagicVoltage m_angleVoltageSetter = new MotionMagicVoltage(0);
+
+    private final VelocityVoltage m_velocityVoltageSetter = new VelocityVoltage(0);
+
+    private TalonFXConfiguration configuration = new TalonFXConfiguration();
+
+    private double targetPosition = Double.NaN;
+
+    private double targetVelocity = Double.NaN;
+
+    private double targetOutput = Double.NaN;
+
+    private boolean factoryDefaultOcurred = false;
+
+    private CustomDoubleLogger appliedOutputLog;
+
+    private CustomDoubleLogger targetOutputLog;
+
+    private CustomDoubleLogger currentLog;
+
+    private CustomDoubleLogger positionLog;
+
+    private CustomDoubleLogger velocityLog;
+
+    private CustomDoubleLogger temperatureLog;
+
+    private CustomIntegerLogger faultsLog;
+
+    private CustomDoubleLogger targetPositionLog;
+
+    private CustomDoubleLogger targetSpeedLog;
+
+    private void setupLogs(int motorId) {
+        this.appliedOutputLog = new CustomDoubleLogger("/motors/" + motorId + "/appliedOutput");
+    this.targetOutputLog = new CustomDoubleLogger("/motors/" + motorId + "/targetOutput");
+    this.currentLog = new CustomDoubleLogger("/motors/" + motorId + "/current");
+    this.positionLog = new CustomDoubleLogger("/motors/" + motorId + "/position");
+    this.velocityLog = new CustomDoubleLogger("/motors/" + motorId + "/velocity");
+    this.temperatureLog = new CustomDoubleLogger("/motors/" + motorId + "/temperature");
+    this.faultsLog = new CustomIntegerLogger("/motors/" + motorId + "/faults");
+    this.targetPositionLog = new CustomDoubleLogger("/motors/" + motorId + "/targetPosition");
+    this.targetSpeedLog = new CustomDoubleLogger("/motors/" + motorId + "/targetSpeed");
+    StringLogEntry descriptionLog = new StringLogEntry(DataLogManager.getLog(),
+        "/motors/" + motorId + "/Description");
+    descriptionLog.append(this.motor.getDescription());
+    BooleanLogEntry isInvertedLog = new BooleanLogEntry(DataLogManager.getLog(), "/motors/" + motorId + "/isInverted");
+    isInvertedLog.append(this.motor.getInverted());
+  }
+
+  @Override
+  public void factoryDefault() {
+    if (!factoryDefaultOcurred) {
+      configuration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+      StatusCode status = motor.getConfigurator().apply(this.configuration, 100);
+      if (status.isError()) {
+        status = motor.getConfigurator().apply(this.configuration, 100);
+      }
+      m_angleVoltageSetter.UpdateFreqHz = 0;
+      m_velocityVoltageSetter.UpdateFreqHz = 0;
+    }
+    factoryDefaultOcurred = true;
+  }
+
+  @Override
+  public void clearStickyFaults() {
+    motor.clearStickyFaults();
+  }
+
+  /**
+   * Set the CAN status frames.
+   *
+   * @param CANStatus1       Applied Motor Output, Fault Information, Limit Switch
+   *                         Information
+   * @param CANStatus2       Selected Sensor Position (PID 0), Selected Sensor
+   *                         Velocity (PID 0), Brushed Supply Current
+   *                         Measurement, Sticky Fault Information
+   * @param CANStatus3       Quadrature Information
+   * @param CANStatus4       Analog Input, Supply Battery Voltage, Controller
+   *                         Temperature
+   * @param CANStatus8       Pulse Width Information
+   * @param CANStatus10      Motion Profiling/Motion Magic Information
+   * @param CANStatus12      Selected Sensor Position (Aux PID 1), Selected Sensor
+   *                         Velocity (Aux PID 1)
+   * @param CANStatus13      PID0 (Primary PID) Information
+   * @param CANStatus14      PID1 (Auxiliary PID) Information
+   * @param CANStatus21      Integrated Sensor Position (Talon FX), Integrated
+   *                         Sensor Velocity (Talon FX)
+   * @param CANStatusCurrent Brushless Supply Current Measurement, Brushless
+   *                         Stator Current Measurement
+   */
+  public void configureCANStatusFrames(
+      int CANStatus1,
+      int CANStatus2,
+      int CANStatus3,
+      int CANStatus4,
+      int CANStatus8,
+      int CANStatus10,
+      int CANStatus12,
+      int CANStatus13,
+      int CANStatus14,
+      int CANStatus21,
+      int CANStatusCurrent) {
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, CANStatus1);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0,
+    // CANStatus2);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature,
+    // CANStatus3);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat,
+    // CANStatus4);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_8_PulseWidth,
+    // CANStatus8);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_Targets,
+    // CANStatus10);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_12_Feedback1,
+    // CANStatus12);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0,
+    // CANStatus13);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_14_Turn_PIDF1,
+    // CANStatus14);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated,
+    // CANStatus21);
+    // motor.setStatusFramePeriod(StatusFrameEnhanced.Status_Brushless_Current,
+    // CANStatusCurrent);
+
+    // TODO: Configure Status Frame 2 thru 21 if necessary
+    // https://v5.docs.ctr-electronics.com/en/stable/ch18_CommonAPI.html#setting-status-frame-periods
+  }
+
+  @Override
+  public void configurePIDF(double P, double I, double D, double F, double Izone) {
+    configurePIDF(P, I, D, F);
+  }
+
+  @Override
+  public void configurePIDF(double P, double I, double D, double F) {
+    TalonFXConfigurator cfg = motor.getConfigurator();
+    cfg.refresh(configuration.Slot0);
+    cfg.apply(
+        configuration.Slot0.withKP(P).withKI(I).withKD(D).withKS(F).withGravityType(gravityType));
+  }
+
+  @Override
+  public void configureFeedForward(double Kg, double Ks, double Kv) {
+    TalonFXConfigurator cfg = motor.getConfigurator();
+    cfg.refresh(configuration.Slot0);
+    cfg.apply(
+        configuration.Slot0.withKG(Kg).withKS(Ks).withKV(Kv));
+  }
+
+  @Override
+  public void configurePIDWrapping(double minInput, double maxInput) {
+    TalonFXConfigurator cfg = motor.getConfigurator();
+    cfg.refresh(configuration.ClosedLoopGeneral);
+    configuration.ClosedLoopGeneral.ContinuousWrap = true;
+    cfg.apply(configuration.ClosedLoopGeneral);
+  }
+
+  @Override
+  public void setMotorBrake(boolean isBrakeMode) {
+    motor.setNeutralMode(isBrakeMode ? NeutralModeValue.Brake : NeutralModeValue.Coast);
+  }
+
+  @Override
+  public void setInverted(boolean inverted) {
+    motor.setInverted(inverted);
+  }
+
+  @Override
+  public void setInvertedEncoder(boolean inverted) {
+    //
+  }
+
+  @Override
+  public void burnFlash() {
+    // Do nothing
+  }
+
+  @Override
+  public void set(double percentOutput) {
+    targetOutput = percentOutput;
+    targetPosition = Double.NaN;
+    targetVelocity = Double.NaN;
+    motor.set(percentOutput);
+  }
+
+  @Override
+  public void setPositionReference(double position) {
+    setPositionReferenceArbFF(position, 0);
+  }
+
+  @Override
+  public void setPositionReferenceArbFF(double position, double feedforward) {
+    double positionInRotations = Units.degreesToRotations(position);
+    targetPosition = position;
+    targetOutput = Double.NaN;
+    targetVelocity = Double.NaN;
+    motor.setControl(new PositionDutyCycle(positionInRotations).withFeedForward(feedforward));
+  }
+
+  @Override
+  public void setPositionReferenceMotionProfiling(double position, double velocity, double feedforward) {
+    double positionInRotations = Units.degreesToRotations(position);
+    targetPosition = position;
+    targetOutput = Double.NaN;
+    targetVelocity = Double.NaN;
+    motor.setControl(new PositionDutyCycle(positionInRotations).withFeedForward(feedforward).withVelocity(velocity));
+  }
+
+  @Override
+  public void setVelocityReference(double velocity, double feedforward) {
+    targetVelocity = velocity;
+    targetOutput = Double.NaN;x
+    targetPosition = Double.NaN;
+    motor.setControl(new VelocityVoltage(velocity).withFeedForward(feedforward));
+  }
+
+  @Override
+  public double getVoltage() {
+    return motor.getMotorVoltage().getValueAsDouble();
+  }
+
+  @Override
+  public void setVoltage(double voltage) {
+    motor.setVoltage(voltage);
+  }
+
+  @Override
+  public double getAppliedOutput() {
+    return motor.getDutyCycle().getValueAsDouble();
+  }
+
+  @Override
+  public double getVelocity() {
+    return motor.getVelocity().getValueAsDouble();
+  }
+
+  @Override
+  public double getPosition() {
+    return Units.rotationsToDegrees(motor.getPosition().getValueAsDouble());
+  }
+
+  @Override
+  public void setPositionFactor(double factor) {
+    TalonFXConfigurator cfg = motor.getConfigurator();
+    configuration.Feedback.withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
+        .withSensorToMechanismRatio(factor);
+    cfg.apply(configuration);
+  }
+
+  @Override
+  public void setVelocityFactor(double factor) {
+    TalonFXConfigurator cfg = motor.getConfigurator();
+    configuration.Feedback.withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
+        .withSensorToMechanismRatio(factor);
+    cfg.apply(configuration);
+  }
+
+  @Override
+  public void setPosition(double position) {
+    motor.setPosition(position);
+  }
+
+  @Override
+  public void setVoltageCompensation(double nominalVoltage) {
+    // Do not implement
+  }
+
+  @Override
+  public void setCurrentLimit(int currentLimit) {
+    TalonFXConfigurator cfg = motor.getConfigurator();
+    cfg.refresh(configuration.CurrentLimits);
+    cfg.apply(
+        configuration.CurrentLimits.withStatorCurrentLimit(currentLimit)
+            .withStatorCurrentLimitEnable(true));
+  }
+
+  @Override
+  public void setLoopRampRate(double rampRate) {
+    TalonFXConfigurator cfg = motor.getConfigurator();
+    cfg.refresh(configuration.ClosedLoopRamps);
+    cfg.apply(configuration.ClosedLoopRamps.withVoltageClosedLoopRampPeriod(rampRate));
+  }
+
+  @Override
+  public Object getMotor() {
+    return motor;
+  }
+
+  @Override
+  public void updateLogs() {
+    this.appliedOutputLog.append(this.motor.getDutyCycle().getValueAsDouble());
+    this.currentLog.append(this.motor.getStatorCurrent().getValueAsDouble());
+    this.positionLog.append(Units.rotationsToDegrees(this.motor.getPosition().getValueAsDouble()));
+    this.velocityLog.append(this.motor.getVelocity().getValueAsDouble());
+    this.temperatureLog.append(this.motor.getDeviceTemp().getValueAsDouble());
+    this.faultsLog.append(this.motor.getFaultField().getValue());
+    this.targetPositionLog.append(this.targetPosition);
+    this.targetSpeedLog.append(this.targetVelocity);
+  }
+
+}
