c50807b136f00aacaacf32db52b34ae70707d8ec
Carvalho <lucacarvalhojeo@gmail.com>
2025-01-13 22:32:31 -0300

    feat: add more swerve ctre project classes

--- src/main/java/frc/Java_Is_UnderControl/Swerve/BaseSwerveSubsystem.java
+++ src/main/java/frc/Java_Is_UnderControl/Swerve/BaseSwerveSubsystem.java
@@ -163,7 +163,7 @@ public abstract class BaseSwerveSubsystem extends TunerSwerveDrivetrain implemen
           this));
 
   /* The SysId routine to test */
-  private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineTranslation;
+  private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineSteer;
 
   protected BaseSwerveSubsystem(BaseSwerveConfig config,
       SwerveDrivetrainConstants drivetrainConstants, double odometryUpdateFrequency,
