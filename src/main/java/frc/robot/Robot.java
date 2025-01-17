9f3e1b26a2d35faff6523e30b6ca537180de6357
Carvalho <lucacarvalhojeo@gmail.com>
2025-01-17 09:32:22 -0300

    feat: swerve tuning with path

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,16 +23,20 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   @Override
-  public void disabledExit() {}
+  public void disabledExit() {
+  }
 
   @Override
   public void autonomousInit() {
+    CommandScheduler.getInstance().cancelAll();
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
@@ -41,10 +45,12 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
-  public void autonomousExit() {}
+  public void autonomousExit() {
+  }
 
   @Override
   public void teleopInit() {
@@ -54,10 +60,12 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
-  public void teleopExit() {}
+  public void teleopExit() {
+  }
 
   @Override
   public void testInit() {
@@ -65,8 +73,10 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   @Override
-  public void testExit() {}
+  public void testExit() {
+  }
 }
--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -45,13 +45,16 @@ public class TunerConstants {
   // the
   // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
   private static final Slot0Configs steerGains = new Slot0Configs()
-      .withKP(100).withKI(0).withKD(0.5)
-      .withKS(0.1).withKV(2.66).withKA(0)
+      // .withKP(100).withKI(0).withKD(0.5)
+      // .withKS(0.1).withKV(2.66).withKA(0)
+      .withKP(163.96).withKI(0).withKD(0.5)
+      .withKS(0.1033).withKV(2.609).withKA(0.04039)
       .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains = new Slot0Configs()
       // .withKP(0.15521).withKI(0).withKD(0)
+      // 0.0018456)
       .withKP(0.0018456).withKI(0).withKD(0)
       .withKS(0.17212).withKV(0.11909);
 
@@ -99,15 +102,16 @@ public class TunerConstants {
 
   // Theoretical free speed (m/s) at 12 V applied output;
   // This needs to be tuned to your individual robot
-  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.76);
+  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.3);
 
   // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
   // This may need to be tuned to your individual robot
-  private static final double kCoupleRatio = 3.125;
+  // 2.78
+  private static final double kCoupleRatio = 2.78;
 
   private static final double kDriveGearRatio = 5.357142857142857;
   private static final double kSteerGearRatio = 21.428571428571427;
-  private static final Distance kWheelRadius = Inches.of(2);
+  private static final Distance kWheelRadius = Inches.of(1.918728097);
 
   private static final boolean kInvertLeftSide = false;
   private static final boolean kInvertRightSide = true;
