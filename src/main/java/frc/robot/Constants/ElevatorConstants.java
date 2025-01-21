0a098f1626098b0b1c1458141d002b88b0447f49
Giovanna <laqi775@gmail.com>
2025-01-21 19:45:27 -0300

    feat: elevator and intake constants

--- src/main/java/frc/robot/Constants/ElevatorConstants.java
+++ src/main/java/frc/robot/Constants/ElevatorConstants.java
@@ -1,5 +1,21 @@
 package frc.robot.Constants;
 
 public class ElevatorConstants {
+  public static final int ID_elevatorMotor1 = 0;
+  public static final int ID_elevatorMotor2 = 0;
 
+  public class tunning_values_elevator {
+    public static final double KS = 0;
+    public static final double KV = 0;
+    public static final double MAX_VELOCITY = 0;
+    public static final double MAX_ACCELERATION = 0;
+
+    public class PID {
+      public static final double P = 0;
+      public static final double I = 0;
+      public static final double D = 0;
+      public static final double F = 0;
+      public static final double IZone = 0;
+    }
+  }
 }
