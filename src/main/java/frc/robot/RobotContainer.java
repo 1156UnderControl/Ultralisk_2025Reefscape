fd8c157a47a1b2991d29f18da73f0d7bf67efffd
KlausKollerKroeff <kroeffk@icloud.com>
2025-01-21 10:35:39 -0300

    feat: run commands by the subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.Java_Is_UnderControl.Joysticks.Types.TypeXboxController;
-import frc.robot.commands.DynamicForward;
-import frc.robot.commands.DynamicReverse;
-import frc.robot.commands.QuasistaticForward;
-import frc.robot.commands.QuasistaticReverse;
 import frc.robot.subsystems.ArmSubsystem;
 
 public class RobotContainer {
@@ -19,16 +15,16 @@ public class RobotContainer {
   ArmSubsystem armSubsystem;
 
   public RobotContainer() {
-    configureBindings();
-    controllerType = new TypeXboxController(0);
     armSubsystem = ArmSubsystem.getInstance();
+    controllerType = new TypeXboxController(0);
+    configureBindings();
   }
 
   private void configureBindings() {
-    controllerType.buttomUp().onTrue(new QuasistaticForward(armSubsystem));
-    controllerType.buttomDown().onTrue(new QuasistaticReverse(armSubsystem));
-    controllerType.buttomRight().onTrue(new DynamicForward(armSubsystem));
-    controllerType.buttomLeft().onTrue(new DynamicReverse(armSubsystem));
+    controllerType.buttomUp().onTrue(this.armSubsystem.sysIdQuasistaticForward());
+    controllerType.buttomDown().onTrue(this.armSubsystem.sysIdQuasistaticReverse());
+    controllerType.buttomRight().onTrue(this.armSubsystem.sysIdDynamicForward());
+    controllerType.buttomLeft().onTrue(this.armSubsystem.sysIdDynamicReverse());
   }
 
   public Command getAutonomousCommand() {
