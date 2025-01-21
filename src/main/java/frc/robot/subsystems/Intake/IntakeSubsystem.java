24f2f1022eb614004bc726a3ce823b477320024a
Giovanna <laqi775@gmail.com>
2025-01-21 16:41:24 -0300

    feat: add Motors

--- src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -1,8 +1,14 @@
 package frc.robot.subsystems.Intake;
 
-public class IntakeSubsystem {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.Java_Is_UnderControl.Motors.IMotor;
+import frc.Java_Is_UnderControl.Motors.SparkMAXMotor;
+
+public class IntakeSubsystem extends SubsystemBase implements IIntake {
+  private IMotor intakeMotor;
 
   public IntakeSubsystem() {
-
+    this.intakeMotor = new SparkMAXMotor(0);
   }
+
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -1,9 +1,16 @@
 package frc.robot.subsystems.elevator;
 
-public class ElevatorSubsystem {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.Java_Is_UnderControl.Motors.IMotor;
+import frc.Java_Is_UnderControl.Motors.SparkMAXMotor;
+
+public class ElevatorSubsystem extends SubsystemBase implements IElevator {
+  private IMotor elevatorMotor1;
+  private IMotor elevatorMotor2;
 
   public ElevatorSubsystem() {
-
+    this.elevatorMotor1 = new SparkMAXMotor(0);
+    this.elevatorMotor2 = new SparkMAXMotor(0);
   }
 
 }
