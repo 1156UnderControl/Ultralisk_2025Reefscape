c58485befc3c46c334ec547035225c0b64e5f65d
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2025-01-26 02:14:40 -0300

    feat: delete elevator subsystem class and add to scorer

--- src/main/java/frc/robot/subsystems/scorer/ScorerSubsystem.java
+++ src/main/java/frc/robot/subsystems/scorer/ScorerSubsystem.java
@@ -2,17 +2,42 @@ package frc.robot.subsystems.scorer;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.Java_Is_UnderControl.Motors.IMotor;
+import frc.Java_Is_UnderControl.Motors.SparkFlexMotor;
 import frc.Java_Is_UnderControl.Motors.SparkMAXMotor;
-import frc.Java_Is_UnderControl.Motors.TalonFXMotor;
+import frc.robot.constants.ElevatorConstants;
+import frc.robot.constants.ScorerConstants;
 
 public class ScorerSubsystem extends SubsystemBase implements IScorer {
 
-  private final IMotor pivotMotor;
-  private final IMotor scoreMotor;
+  private IMotor elevatorMotorLeader = new SparkFlexMotor(ElevatorConstants.ID_elevatorLeaderMotor, "ELEVATOR_MASTER");
+  private IMotor elevatorMotorFollower = new SparkFlexMotor(ElevatorConstants.ID_elevatorFollowerMotor,
+      "ELEVATOR_FOLLOWER");
+
+  private final IMotor pivotMotor = new SparkMAXMotor(ScorerConstants.ID_pivotMotor, true, "PIVOT");
+  private final IMotor endEffectorMotor = new SparkMAXMotor(ScorerConstants.ID_endEffectorMotor, "END_EFFECTOR");
 
   public ScorerSubsystem() {
-    this.pivotMotor = new TalonFXMotor(0);
-    this.scoreMotor = new SparkMAXMotor(0);
+    setConfigsElevator();
+  }
+
+  private void setConfigsElevator() {
+    elevatorMotorLeader.setMotorBrake(true);
+    elevatorMotorFollower.setMotorBrake(true);
+    elevatorMotorLeader.setLoopRampRate(0.5);
+    elevatorMotorFollower.setLoopRampRate(0.5);
+    elevatorMotorFollower.setFollower(ElevatorConstants.ID_elevatorLeaderMotor, true);
+    elevatorMotorLeader
+        .setPositionFactor(ElevatorConstants.POSITION_FACTOR_MOTOR_ROT_TO_METERS);
+    elevatorMotorLeader.configureMotionProfiling(
+        ElevatorConstants.tunning_values_elevator.PID.P,
+        ElevatorConstants.tunning_values_elevator.PID.I,
+        ElevatorConstants.tunning_values_elevator.PID.D,
+        ElevatorConstants.tunning_values_elevator.PID.F,
+        ElevatorConstants.tunning_values_elevator.MAX_VELOCITY,
+        ElevatorConstants.tunning_values_elevator.MAX_ACCELERATION,
+        ElevatorConstants.tunning_values_elevator.POSITION_ERROR_ALLOWED);
+    elevatorMotorFollower.burnFlash();
+    elevatorMotorLeader.burnFlash();
   }
 
   @Override
@@ -21,32 +46,36 @@ public class ScorerSubsystem extends SubsystemBase implements IScorer {
   }
 
   @Override
-  public void intake(){}
-
-
-
-  @Override
-  public void intakeFromHP(){}
-
+  public void intake() {
+  }
 
   @Override
-  public void prepareToPlaceL1() {}
-
-
-  @Override
-  public void prepareToPlaceL2() {}
+  public void intakeFromHP() {
+  }
 
   @Override
-  public void prepareToPlaceL3() {}
+  public void prepareToPlaceL1() {
+  }
 
   @Override
-  public void prepareToPlaceL4() {}
+  public void prepareToPlaceL2() {
+  }
 
   @Override
-  public void place() {}
+  public void prepareToPlaceL3() {
+  }
 
   @Override
-  public void expell() {}
+  public void prepareToPlaceL4() {
+  }
+
+  @Override
+  public void place() {
+  }
+
+  @Override
+  public void expell() {
+  }
 
   @Override
   public boolean isSecuredToPlace() {
@@ -59,11 +88,11 @@ public class ScorerSubsystem extends SubsystemBase implements IScorer {
   }
 
   @Override
-  public void stopIntaking() {}
+  public void stopIntaking() {
+  }
 
   @Override
   public boolean isAtSetPoint() {
     return false;
   }
 }
-
