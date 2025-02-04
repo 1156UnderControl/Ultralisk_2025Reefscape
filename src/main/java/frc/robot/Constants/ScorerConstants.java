9fb6a56886030608f2e6da4f606a13e18e67a079
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2025-02-04 14:06:33 -0300

    feat: pivot constants

--- src/main/java/frc/robot/Constants/ScorerConstants.java
+++ src/main/java/frc/robot/Constants/ScorerConstants.java
@@ -4,6 +4,9 @@ public class ScorerConstants {
   public static final int ID_pivotMotor = 0;
   public static final int ID_endEffectorMotor = 0;
 
+  public static final double POSITION_FACTOR_MOTOR_ROTATION_TO_MECHANISM_DEGREES = 0;
+  public static final double VELOCITY_FACTOR_MOTOR_RPM_TO_MECHANISM_RPM = 0;
+
   public class tunning_values_scorer {
     public static final double KS = 0;
     public static final double KV = 0;
