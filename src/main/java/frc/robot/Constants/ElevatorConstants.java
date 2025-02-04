e19aab45e32003a18114ff42e24872c70cf40cb1
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2025-02-04 14:07:14 -0300

    chore: rename elevator factor constants

--- src/main/java/frc/robot/Constants/ElevatorConstants.java
+++ src/main/java/frc/robot/Constants/ElevatorConstants.java
@@ -3,7 +3,7 @@ package frc.robot.constants;
 public class ElevatorConstants {
   public static final int ID_elevatorLeaderMotor = 0;
   public static final int ID_elevatorFollowerMotor = 0;
-  public static final double POSITION_FACTOR_MOTOR_ROT_TO_METERS = 0;
+  public static final double POSITION_FACTOR_MOTOR_ROTATION_TO_MECHANISM_METERS = 0;
 
   public class tunning_values_elevator {
     public static final double KS = 0;
