6c9d8344a7f8f3fe49b433802f0c88a0975dad8d
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2025-02-05 23:41:04 -0300

    chore: separate end effector and pivot constants from scorer constants

--- src/main/java/frc/robot/Constants/PivotConstants.java
+++ src/main/java/frc/robot/Constants/PivotConstants.java
@@ -1,13 +1,11 @@
 package frc.robot.constants;
 
-public class ScorerConstants {
+public class PivotConstants {
   public static final int ID_pivotMotor = 0;
-  public static final int ID_endEffectorMotor = 0;
-
   public static final double POSITION_FACTOR_MOTOR_ROTATION_TO_MECHANISM_DEGREES = 0;
-  public static final double VELOCITY_FACTOR_MOTOR_RPM_TO_MECHANISM_RPM = 0;
+  public static final double VELOCITY_FACTOR_MOTOR_RPM_TO_MECHANISM_DEG_PER_SECOND = 0;
 
-  public class tunning_values_scorer {
+  public class tunning_values_pivot {
     public static final double KS = 0;
     public static final double KV = 0;
     public static final double MAX_VELOCITY = 0;
@@ -25,9 +23,6 @@ public class ScorerConstants {
     public class setpoints {
       public static final double MAX_ANGLE_PIVOT = 2.5;
       public static final double MIN_ANGLE_PIVOT = 0;
-
-      public static final double DUTY_CYCLE_INTAKE = 1.0;
-      public static final double DUTY_CYCLE_EXPELL = -1.0;
     }
   }
 }
