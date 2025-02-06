c289cdfa916a4e6be8c841d936f91d935a09a2ec
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2025-02-05 23:41:38 -0300

    chore: change intake constants

--- src/main/java/frc/robot/Constants/IntakeConstants.java
+++ src/main/java/frc/robot/Constants/IntakeConstants.java
@@ -1,19 +1,13 @@
 package frc.robot.constants;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-
 public class IntakeConstants {
-  public static final Pose2d intakeRollerPositionRelativeToRobotCenter = new Pose2d(0.46, 0, new Rotation2d());
   public static final int ID_intakeMotor = 0;
   public static final int port_IR = 0;
 
   public class tunning_values_intake {
     public class setpoints {
-      public static final double SPEED_INTAKE = 0;
-      public static final double SPEED_EXPELL = 0;
+      public static final double SPEED_INTAKE = 1;
+      public static final double SPEED_EXPELL = -1;
     }
   }
-
-  public static Object setpoints;
 }
