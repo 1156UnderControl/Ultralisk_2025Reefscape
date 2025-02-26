4c5b76f3b3a8975fab5b9960f4b8107979bc9410
KlausKollerKroeff <kroeffk@icloud.com>
2025-02-26 10:41:24 -0300

    feat: led color on intake commands

--- src/main/java/frc/robot/commands/intake/CollectCoralFromHP.java
+++ src/main/java/frc/robot/commands/intake/CollectCoralFromHP.java
@@ -1,10 +1,13 @@
 package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.Java_Is_UnderControl.LEDs.LedColor;
+import frc.Java_Is_UnderControl.LEDs.LedSubsystem;
 import frc.robot.SuperStructure;
 
 public class CollectCoralFromHP extends Command {
   SuperStructure superStructure;
+  LedSubsystem led = LedSubsystem.getInstance();
 
   public CollectCoralFromHP(SuperStructure superStructure) {
     this.superStructure = superStructure;
@@ -15,11 +18,17 @@ public class CollectCoralFromHP extends Command {
   public void initialize() {
     superStructure.intake.intake();
     superStructure.scorer.intakeFromHP();
+    led.setSolidColor(LedColor.RED);
   }
 
   @Override
   public void execute() {
     superStructure.scorer.intakeFromHP();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    led.setSolidColor(LedColor.GREEN);
   }
 
   @Override
