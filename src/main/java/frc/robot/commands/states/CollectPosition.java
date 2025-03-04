33c65bf703b7a6f8e6c2f46e182dfc358a403a32
KlausKollerKroeff <kroeffk@icloud.com>
2025-03-03 22:28:57 -0300

    feat: remove intake subsystem

--- src/main/java/frc/robot/commands/states/CollectPosition.java
+++ src/main/java/frc/robot/commands/states/CollectPosition.java
@@ -3,7 +3,6 @@ package frc.robot.commands.states;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.SuperStructure;
-import frc.robot.commands.intake.CollectCoralFromHP;
 import frc.robot.commands.scorer.MoveScorerToCollectPosition;
 import frc.robot.commands.swerve.SwerveAngleWithCoralStation;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
@@ -12,7 +11,6 @@ public class CollectPosition extends SequentialCommandGroup {
 
   public CollectPosition(SuperStructure superStructure, SwerveSubsystem swerve) {
     addCommands(Commands.race(new MoveScorerToCollectPosition(superStructure), new SwerveAngleWithCoralStation(swerve)),
-        Commands.race(new CollectCoralFromHP(superStructure), new SwerveAngleWithCoralStation(swerve)),
         Commands.waitSeconds(0.2));
   }
 }
