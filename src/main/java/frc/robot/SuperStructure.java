b5dd55e1cea184e36cae3d553211e8bc72e5af9f
KlausKollerKroeff <kroeffk@icloud.com>
2025-03-06 18:11:28 -0300

    feat: exclude intake

--- src/main/java/frc/robot/SuperStructure.java
+++ src/main/java/frc/robot/SuperStructure.java
@@ -7,14 +7,11 @@ import frc.Java_Is_UnderControl.Logging.EnhancedLoggers.CustomStringLogger;
 import frc.robot.joysticks.ControlBoard;
 import frc.robot.subsystems.climber.ClimberSubsystem;
 import frc.robot.subsystems.climber.IClimber;
-import frc.robot.subsystems.intake.IIntake;
-import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.scorer.IScorer;
 import frc.robot.subsystems.scorer.ScorerSubsystem;
 
 public class SuperStructure extends SubsystemBase {
   public IScorer scorer;
-  public IIntake intake;
   public IClimber climber;
 
   private ControlBoard controlBoard = ControlBoard.getInstance();
@@ -33,7 +30,6 @@ public class SuperStructure extends SubsystemBase {
 
   public SuperStructure() {
     this.scorer = ScorerSubsystem.getInstance();
-    this.intake = IntakeSubsystem.getInstance();
     this.climber = ClimberSubsystem.getInstance();
     this.powerDistributionHub = new PowerDistribution();
     this.batteryVoltageLogEntry.append(this.powerDistributionHub.getVoltage());
@@ -43,20 +39,15 @@ public class SuperStructure extends SubsystemBase {
   @Override
   public void periodic() {
     this.scorer.periodic();
-    this.intake.periodic();
     this.climber.periodic();
     this.autoStateLogger.append(this.auto_State);
     this.batteryVoltageLogEntry.append(this.powerDistributionHub.getVoltage());
     this.totalCurrentDrawLogEntry.append(this.powerDistributionHub.getTotalCurrent());
   }
 
-  // public boolean scorerHasCoral() {
-  // return this.scorer.hasCoral();
-  // }
-
-  // public boolean intakeHasCoral() {
-  // return this.scorer.hasCoral();
-  // }
+  public boolean scorerHasCoral() {
+    return this.scorer.hasCoral();
+  }
 
   public void setCoastToRobot() {
     this.scorer.setCoastScorer();
@@ -68,12 +59,4 @@ public class SuperStructure extends SubsystemBase {
     this.climber.setBrakeClimber();
   }
 
-  public boolean isRobotAbleToScore() {
-    // if (this.controlBoard.getDoNotScore()) {
-    return false;
-    // }
-    // return this.scorer.isPivotAtSetPointForAutoScore()
-    // && this.scorer.isElevatorAtSetPointForScoring();
-    // }
-  }
 }
