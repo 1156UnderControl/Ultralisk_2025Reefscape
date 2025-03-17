a79e56c13531e0221c357dbe7fcf79f867da3af9
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2025-03-17 19:16:20 -0300

    feat: restart timer

--- src/main/java/frc/robot/commands/autonomous_commands/CollectAutonomousOpitimized.java
+++ src/main/java/frc/robot/commands/autonomous_commands/CollectAutonomousOpitimized.java
@@ -1,20 +1,24 @@
 package frc.robot.commands.autonomous_commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.SuperStructure;
 
 public class CollectAutonomousOpitimized extends Command {
   SuperStructure superStructure;
+  Timer collectTimer;
 
   public CollectAutonomousOpitimized(SuperStructure superStructure) {
     this.superStructure = superStructure;
     addRequirements(this.superStructure);
+    collectTimer = new Timer();
   }
 
   @Override
   public void initialize() {
     this.superStructure.auto_State = "COLLECTING";
     superStructure.scorer.intakeFromHP();
+    collectTimer.restart();
   }
 
   @Override
@@ -24,11 +28,16 @@ public class CollectAutonomousOpitimized extends Command {
 
   @Override
   public boolean isFinished() {
+    if (collectTimer.get() >= 1) {
+      return true;
+    }
     return superStructure.scorer.hasCoral();
   }
 
   @Override
   public void end(boolean interrupted) {
-    superStructure.scorer.stopEndEffector();
+    collectTimer.stop();
+    collectTimer.reset();
+    // superStructure.scorer.stopEndEffector();
   }
 }
