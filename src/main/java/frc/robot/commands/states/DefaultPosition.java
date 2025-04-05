3eb7e4eeb069a7a77b3733b98b683b1d8a76f758
kalsing <gabrilel.kalsing@gmail.com>
2025-04-05 18:10:00 -0300

    feat: new led colors for default postion

--- src/main/java/frc/robot/commands/states/DefaultPosition.java
+++ src/main/java/frc/robot/commands/states/DefaultPosition.java
@@ -3,6 +3,7 @@ package frc.robot.commands.states;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.Java_Is_UnderControl.LEDs.LedColor;
 import frc.robot.SuperStructure;
 import frc.robot.commands.climber.StowClimber;
 import frc.robot.commands.scorer.MoveScorerToDefaultPosition;
@@ -13,6 +14,11 @@ public class DefaultPosition extends SequentialCommandGroup {
     addCommands(new StowClimber(superStructure),
         new MoveScorerToDefaultPosition(superStructure),
         new InstantCommand(() -> superStructure.led.setRainbow()),
+        new InstantCommand(() -> {
+          if (superStructure.scorer.hasCoral()) {
+            superStructure.led.setSolidColor(LedColor.WHITE);
+          }
+        }),
         Commands.idle(superStructure));
   }
 }
